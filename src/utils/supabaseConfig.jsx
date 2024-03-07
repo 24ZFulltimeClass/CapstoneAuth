b9bc62e2194076b4d1e32177db5c5bc223973a98
WalobwaD <walobwadan@gmail.com>
2024-03-07 10:49:07 +0300

    hiding keys

--- src/utils/supabaseConfig.jsx
+++ src/utils/supabaseConfig.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js/"
 
-const supaBaseUrl = "https://pmjwikmxvxukgfrayekj.supabase.co"
-const supaBaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtandpa214dnh1a2dmcmF5ZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyNzgwNTgsImV4cCI6MjAyNDg1NDA1OH0.OKPutR5Z4QXKxx0CReuKpO20yvtTFVqXvp51N9rAksI"
+const supaBaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supaBaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supaBaseUrl, supaBaseAnonKey)
